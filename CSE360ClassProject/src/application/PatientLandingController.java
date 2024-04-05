3c673b180d9458dc17f7fa088b0cc6fc5e94b5cc
John M <142964822+JAM7676@users.noreply.github.com>
2024-04-04 17:23:01 -0700

    Add files via upload
    What was added:
    - Login
    - File and folder creation for patient account info
    - Some added missing screens.
    
    What still needs to be done
    - There are still missing screens, which are all on the nurse and doctor side of things.
    - Storing more data, which are things like previous visits and messages
    - Viewing data, particularly in parts such as "view patient data" and "past visits"
    - Messaging side of things(new file structure may be needed)
    - Probably more I havent thought of.

--- CSE360ClassProject/src/application/PatientLandingController.java
+++ CSE360ClassProject/src/application/PatientLandingController.java
@@ -1,18 +1,27 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class PatientLandingController {
+public class PatientLandingController implements Initializable
+{
 	private Scene scene;
 	private Parent root;
 	private Stage stage;
+	
+	@FXML
+	private Label welcomeLabel;
 	
 	public void switchToLogin(ActionEvent e) throws IOException
 	{
@@ -46,5 +55,10 @@ public class PatientLandingController {
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		welcomeLabel.setText(welcomeLabel.getText() + Main.currUser.lastName);
 	}
 }
